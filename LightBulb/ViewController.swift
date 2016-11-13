fcd955bde0ea22f311fdd494f646e7af7e752849
KPicart <kpicart@stepwisedesigns.com>
2016-11-13 10:26:47 -0500

    completed LightBulb lab

--- LightBulb/ViewController.swift
+++ LightBulb/ViewController.swift
@@ -19,17 +19,30 @@ class ViewController: UIViewController {
         
         lightBulb.backgroundColor = UIColor.blue
         
+               
     }
+    
     
     
     @IBAction func colorSelected(_ sender: UISegmentedControl) {
         
+        switch sender.selectedSegmentIndex {
+        case 0:
+            lightBulb.backgroundColor = UIColor.blue
+        case 1:
+            lightBulb.backgroundColor = UIColor.red
+        case 2:
+            lightBulb.backgroundColor = UIColor.green
+        case 3:
+            lightBulb.backgroundColor = UIColor.yellow
+            
+        default:
+            lightBulb.backgroundColor = UIColor.black
+            
+        }
         print(sender.selectedSegmentIndex)
         
     }
     
     
-
-   
-
 }
